4de1750b326685f17fdc13ec559c5a604ecdfdcc
aatayde <alejandro.atayde.jr@gmail.com>
2023-04-01 18:26:02 -0700

    add global variables to manage Unit Symbol & Units of measurements

--- script.js
+++ script.js
@@ -14,14 +14,16 @@ weather = {
         console.log(name, icon, description, temp, humidity, speed)
         document.querySelector('.city').innerText = name;
         document.querySelector('.icon').src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
-        document.querySelector('.temp').innerText =  temp.toFixed(0) + "° C";
+        document.querySelector('.temp').innerText =  temp.toFixed(0) + "° " + unitSymbol;
         document.querySelector('.desctiption').innerText =  description;
         document.querySelector('.wind').innerText = "wind " + speed + " mph";
         document.querySelector('.humidity').innerText = "humidity " + humidity + "%";
         document.body.style.backgroundImage = "url('https://source.unsplash.com/random/?" + name +"')";
     },
     search: function(){
-        this.fetchWeather(document.querySelector('.search-bar').value , this.getUnits())
+        console.log(testName)
+        this.fetchWeather(document.querySelector('.search-bar').value , this.getUnits(testName))
+       
     },
     clearSearchText: function(){
         document.querySelector('.search-bar').value = '';
@@ -29,36 +31,50 @@ weather = {
     convertTemperature: function(){
         let temperature = parseFloat(document.querySelector('.temp').innerText)
        
-
-         if (this.isCelcius()){
+         if (this.isCelsius()){
             let farenheit = ((temperature * (9/5)) + 32);
+            this.getUnits('imperial')
             document.querySelector('.temp').innerText = farenheit.toFixed(0) + "° F";
 
          } else {
             let celcius = ((temperature - 32) * (5/9) );
+            this.getUnits('metric')
             document.querySelector('.temp').innerText = celcius.toFixed(0) + "° C";
-         }
-        
+         } 
     },
-    isCelcius: function(){
+    isCelsius: function(){
         return (document.querySelector('.temp').innerText.includes('C') ? true : false)
     },
     getUnits: function(systemOfMeasurements){
+        console.log("what is system of measurements: ", systemOfMeasurements)
+
         switch (systemOfMeasurements){
             case 'metric': 
                 console.log('Metic Selected')
+              
+                unitSymbol = 'C'
+                testName = 'metric';
+                
                 return 'metric';
             case 'imperial' : 
             console.log('Imperial Selected')
+                unitSymbol = 'F'
+                testName = 'imperial';
+               
                 return 'imperial';
             default : 
             console.log('Default Selected')
+                
+                unitSymbol = 'C'
+                testName = 'metric';
+                // break;
                 return 'metric';
         }
     }
-    
-
 }
+
+testName = 'metric';
+unitSymbol = 'C';
 
 // Add Search Buton Feature
 document.querySelector('.search button').addEventListener("click", function(){
@@ -66,7 +82,6 @@ document.querySelector('.search button').addEventListener("click", function(){
     weather.search();
     weather.clearSearchText();
 })
-
 
 
 // Add event listener for city input on keydown
@@ -80,6 +95,6 @@ document.querySelector('.search-bar').addEventListener("keydown", function(event
 
 // add temperature conversion
 document.querySelector('.temp').addEventListener("click", function(){
-
+    
     weather.convertTemperature()
 })
